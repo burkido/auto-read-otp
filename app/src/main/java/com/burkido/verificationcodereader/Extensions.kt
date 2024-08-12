b3c502d7b30e77f278c7bd56bd65a853140be4af
burak <karadumanburak000@gmail.com>
2024-08-12 10:16:24 +0300

    - clearFocusOnTap extension function added

---
+++ app/src/main/java/com/burkido/verificationcodereader/Extensions.kt
@@ -0,0 +1,24 @@
+package com.burkido.verificationcodereader
+
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+
+fun Modifier.clearFocusOnTap(): Modifier = composed {
+    val focusManager = LocalFocusManager.current
+    this.then(Modifier.pointerInput(Unit) {
+        awaitEachGesture {
+            awaitFirstDown(pass = PointerEventPass.Initial)
+            val upEvent = waitForUpOrCancellation(pass = PointerEventPass.Initial)
+            if (upEvent != null) {
+                focusManager.clearFocus()
+            }
+        }
+    })
+}
