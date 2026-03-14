10b1a486f524aa47a1469318066eefae972dd9ab
Burak Karaduman <karadumanburak000@gmail.com>
2026-03-14 18:54:55 +0300

    chore(settings): enable TYPESAFE_PROJECT_ACCESSORS feature preview

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
